c1180eb811dea09f2ce5724de9e6d0f5010766ff
Patrick Meirelles <89944016+PatrickMeirelles@users.noreply.github.com>
2025-01-16 20:57:59 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { server } from './server/Server';
 
-server.listen(process.env.PORT||3333, '0.0.0.0', () => {
-  console.log(`Server running on port: ${process.env.PORT||3333}`);
+const port = Number(process.env.PORT) || 3333;
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port: ${port}`);
 });
